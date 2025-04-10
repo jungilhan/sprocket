dd46f088cae82f7659d97bfd0b6078b124e000bf
jungil.han <jungil.han@gmail.com>
2025-04-10 23:30:47 +0000

    fix: Resolve asset loading issues with base path and build script

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-});
+export default defineConfig(({
+    base: '/sprocket/',
+    plugins: [svelte()],
+    build: {
+      rollupOptions: {
+        output: {},
+      },
+    },
+  }));
