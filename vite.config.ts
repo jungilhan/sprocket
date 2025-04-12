18e6f2b27cb158344f200eb30a7770bfc17a41f2
jungil.han <jungil.han@gmail.com>
2025-04-12 16:05:42 +0000

    style: format code with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-export default defineConfig(({
-    base: '/sprocket/',
-    plugins: [svelte()],
-    build: {
-      rollupOptions: {
-        output: {},
-      },
+export default defineConfig({
+  base: "/sprocket/",
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {},
     },
-  }));
+  },
+});
